81292af37d340afc31caba2e134dd89049b4f035
nriddiford <nickriddiford@me.com>
2017-05-20 08:48:21 +0200

    Write tsv -> write summary

--- script/sv_parse.1.0.pl
+++ script/sv_parse.1.0.pl
@@ -112,8 +112,8 @@ SV_parser::dump_variants( $SVs, $info, $filter, $chromosome ) if $dump;
 # Write out variants passing filters
 SV_parser::print_variants ( $SVs, $filtered_vars, $name, $output_dir ) if $output_dir;
 
-# Write out some useful info to tsv file
-SV_parser::write_tsv ( $SVs, $name, $output_dir, $type ) if $output_dir;
+# Write out some useful info to txt file
+SV_parser::write_summary ( $SVs, $name, $output_dir, $type ) if $output_dir;
 
 sub usage {
   print
