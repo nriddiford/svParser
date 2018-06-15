631c87fbdf729f7adfd477cb3ab4912828512cad
nriddiford <nickriddiford@me.com>
2018-06-15 17:53:19 +0200

    Lots of small changes: -n flag to limit PON print -t to compare filtered vars to truth set

---
+++ script/hashKeys.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use feature qw/ say /;
+use Data::Dumper;
+use Data::Printer;
+
+my %filters;
+
+my @samples = qw /samp1 samp2 samp3 samp4 samp4 samp5 samp6/;
+
+my $info = undef;
+
+say "d" if $info;
