d54698bd056f5b856ba10d7a083fc4dd38fb89c3
nriddiford <nickriddiford@me.com>
2018-07-27 17:48:53 +0200

    Add script to parse CF; Annotate vars with copy number ratios from cnv-seq output

---
+++ script/findCNV.pl
@@ -0,0 +1,106 @@
+#!/usr/bin/perl
+use feature qw/ say /;
+use Data::Printer;
+use autodie;
+use File::Basename;
+use FindBin qw/ $Script /;
+
+use Getopt::Long qw/ GetOptions /;
+
+my $cnv_file;
+my $output_dir;
+my $help;
+my $variants_file;
+
+GetOptions( 'cnvs=s'        =>   \$cnv_file,
+            'variants=s'    =>   \$variants_file,
+            'output_dir=s'  =>   \$output_dir,
+            'help'          =>   \$help
+) or die usage();
+
+if (not $cnv_file) {
+   exit usage();
+}
+
+sub getFreec {
+  my ($file, $c1, $bp1, $bp2) = @_;
+  open my $in, '<', $file;
+  my ($name, $extention) = split(/\.([^.]+)$/, basename($file), 2);
+  say $name;
+  my %cnvs;
+  say "Getting CNV";
+  my ($r, $i) = (0,0);
+
+  while(<$in>){
+    chomp;
+    next if /Chromosome/;
+    next if /^\s+/;
+
+    my ($chr, $start, $ratio, $medratio, $cn) = (split)[0..4];
+    if($c1 eq $chr and $bp1 <= $start and $bp2 >= $start){
+      $r += $ratio;
+      say $ratio;
+      $i++;
+    }
+  }
+  return($r, $i);
+}
+
+sub readCNV {
+  my ($file) = shift;
+  open my $in, '<', $file;
+  my ($name, $extention) = split(/\.([^.]+)$/, basename($file), 2);
+  my %cnvs;
+  say "Getting CNVs from '$name'";
+
+  while(<$in>){
+    chomp;
+    next if /chromosome/;
+    next if /^\s+/;
+    my ($chr, $start, $end, $test, $ref, $p, $log2) = (split)[0..6];
+    $chr =~ s/\"//g;
+    next if $log2 eq 'NA' or $log2 =~ /inf/i;
+    $cnvs{$chr}{$start} = [$end, $test, $ref, $p, $log2];
+  }
+  return(\%cnvs);
+}
+
+
+sub findCNV {
+  my($d, $c1, $bp1, $bp2) = @_;
+
+  my %cnvs = %{ $d };
+  my ($r, $i) = (0, 0);
+  for my $st (keys %{$cnvs{$c1}}){
+    my ($end, $test, $ref, $p, $log2) = @{$cnvs{$c1}{$st}};
+    if($bp1 <= $st and $bp2 >= $end){
+      $r += $log2;
+      $i++;
+    }
+  }
+  return($r, $i);
+}
+
+sub readParser {
+  my ($d, $variants_file) = @_;
+  open my $in, '<', $variants_file;
+  my @header = qw/ source type chromosome1 bp1 chromosome2 bp2 split_reads disc_reads genotype id length(Kb) position consensus microhomology configuration allele_frequency log2(cnv) /;
+  while(<$in>){
+    chomp;
+    next if /^source/;
+    my @parts = split;
+    my ($chr, $bp1, $bp2) = @parts[2,3,5];
+    say "$chr, $bp1, $bp2";
+    my ($r, $i) = findCNV($d, $chr, $bp1, $bp2);
+    my $av_r = sprintf("%.2f", $r/$i);
+    say "Average log2 FC: $av_r over $i windows";
+  }
+}
+
+my $d = readCNV($cnv_file);
+say "Read file '$cnv_file'";
+readParser($d, $variants_file);
+
+sub usage {
+  print "usage: perl $Script -c cnvs -o output_dir\n";
+}
---
+++ script/parseCF.pl
@@ -0,0 +1,80 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use feature qw/ say /;
+use Data::Printer;
+use Data::Dumper;
+use autodie;
+
+use File::Basename;
+use FindBin qw($Bin);
+use FindBin qw/ $Script /;
+
+use Getopt::Long qw/ GetOptions /;
+
+my $cnvs;
+my $output_dir;
+my $help;
+
+GetOptions( 'cnvs=s'        =>   \$cnvs,
+            'output_dir=s'  =>   \$output_dir,
+            'help'          =>   \$help
+) or die usage();
+
+if (not $cnvs or not $output_dir) {
+   exit usage();
+}
+
+my $cnv_ref = extractVars($cnvs);
+
+my @name_fields = split( /\_/, basename($cnvs) );
+
+say @name_fields;
+
+my $outfile =  $name_fields[0] . ".freec.filtered.summary.txt";
+my $outpath = File::Spec->catdir( $output_dir, $outfile );
+open my $out, '>', $outpath;
+
+my @header = qw/ source type chromosome1 bp1 chromosome2 bp2 split_reads disc_reads genotype id length(Kb) position consensus microhomology configuration allele_frequency log2(cnv) /;
+print $out join("\t", @header) . "\n";
+
+my @lines = @{$cnv_ref};
+print $out "$_\n" foreach @lines;
+
+
+sub extractVars {
+  my $f = shift;
+  open my $in, '<', $f;
+  my @cnv;
+  while(<$in>){
+    chomp;
+    next if /KolmogorovSmirnovPvalue/;
+    my ($chrom, $start, $stop, $cn, $type, $wilcox, $KS) = split;
+    $type = ($type eq 'gain') ? 'DUP' : 'DEL';
+    my $length = $stop - $start;
+
+    push @cnv, join("\t", "Control-Freec",         # source
+                            $type,                 # type
+                            $chrom,                # chrom1
+                            $start,                # bp1
+                            $chrom,                # chrom2
+                            $stop,                 # bp2
+                            '-',                   # split reads
+                            '-',                   # paired reads
+                            'somatic_tumour',      # genotype
+                            '-',                   # id
+                            $length,               # length
+                            "$chrom:$start-$stop", # IGV
+                            '-',                   # misc1 (type)
+                            '-',                   # microhomology
+                            '-',                   # configuration
+                            '-',                   # allele frequency
+                            '-');                  # misc (cnv)
+
+  }
+  return(\@cnv);
+}
+
+sub usage {
+  print "usage: perl $Script -c cnvs -o output_dir\n";
+}
