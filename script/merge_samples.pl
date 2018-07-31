07666c07d24e57ba75fc354ff2b136972a979a3e
nriddiford <nickriddiford@me.com>
2018-07-31 18:26:34 +0200

    Cleanup

--- script/merge_samples.pl
+++ script/merge_samples.pl
@@ -14,8 +14,6 @@ open my $all, '>', 'all_samples.txt';
 open my $file1, '<', $ARGV[0];
 my $header = <$file1>;
 close $file1;
-
-# my $header = join("\t", qw/sample event source type chromosome1 bp1 chromosome2 bp2 split_reads pe_reads id length(Kb) position consensus|type microhomology configuration allele_frequency mechanism|log2(cnv) bp1_locus bp2_locus affected_genes T|F notes/);
 print $all "sample" . "\t" . $header . "\n";
 
 my %seen;
