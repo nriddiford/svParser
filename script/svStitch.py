4c4168b79bc614f6b3e02116ee2bb6376d2284c2
nriddiford <nickriddiford@me.com>
2018-10-01 14:07:41 +0200

    Remove 'complex' tag if only 1 class of event (DEL/DUP); some minor improvements

--- script/svStitch.py
+++ script/svStitch.py
@@ -7,7 +7,6 @@ from optparse import OptionParser
 from collections import defaultdict
 import json
 import ntpath
-from collections import OrderedDict
 
 
 def parse_vars(options):
@@ -29,8 +28,8 @@ def parse_vars(options):
     for idx, row in df.iterrows():
         event, c1, b1, c2, b2, notes = row[['event', 'chromosome1', 'bp1', 'chromosome2', 'bp2', 'notes']]
 
-        for i in range(b1-10, b1+10):
-            for j in range(b2-10, b2+10):
+        for i in range(b1 - 10, b1 + 10):
+            for j in range(b2 - 10, b2 + 10):
                 if i in seen_l[c1] and j in seen_r[c2]:
                     index = same_index(seen_l[c1][i], seen_r[c2][j])
                     if index is not None:
@@ -68,19 +67,19 @@ def stitch(right_end, left_end, options):
     complex_events = {}
     seen = []
 
-    complex_events = join_in_window(left_end, window, complex_events)
-    complex_events = join_in_window(right_end, window, complex_events)
+    complex_events = join_same_end(left_end, window, complex_events)
+    complex_events = join_same_end(right_end, window, complex_events)
 
-    for c, b1 in sorted(right_end.iteritems()):
+    for c in sorted(right_end.keys()):
         for b1 in sorted(right_end[c].keys()):
-            for i in range(b1-window, b1+window):
+            for i in range(b1 - window, b1 + window):
                 if i in left_end[c]:
                     e1 = right_end[c][b1][0][0]
                     e2 = left_end[c][i][0][0]
                     if e1 == e2:
                         continue
-                    seen_key = '_'.join(map(str, [e1,e2]))
-                    if seen_key not in seen: print ("Overlap between right event %s [%s] and left event %s [%s]" % (e1, b1, e2, i))
+                    seen_key = '_'.join(map(str, [e1, e2]))
+                    if seen_key not in seen: print("Overlap between right event %s [%s] and left event %s [%s]" % (e1, b1, e2, i))
                     seen.append(seen_key)
                     complex_events.setdefault(e1, []).extend([e1, e2])
 
@@ -88,40 +87,39 @@ def stitch(right_end, left_end, options):
     return pack_complex_vars(complex_events)
 
 
-def join_in_window(d, window, complex_events):
+def join_same_end(d, window, complex_events):
     seen = []
-    for c, data in sorted(d.iteritems()):
+    for c in sorted(d.keys()):
         for b1 in sorted(d[c].keys()):
             seen.append(b1)
             first_event = d[c][b1][0][0]
-            for i, l in enumerate(sorted(d[c][b1])):
-                if first_event != l[0]:
-                    complex_events.setdefault(first_event, []).extend([first_event, l[0]])
-            for i in range(b1-window, b1+window):
-                if i in d[c] and i not in seen:
-                    complex_events.setdefault(first_event, []).extend([first_event, d[c][i][0][0]])
+            for i, var in enumerate(sorted(d[c][b1])):
+                # if first_event == var[0]: continue
+                # complex_events.setdefault(first_event, []).extend([first_event, var[0]])
+                for j in range(b1 - window, b1 + window):
+                    if j in d[c] and j not in seen:
+                        complex_events.setdefault(first_event, []).extend([first_event, d[c][j][0][0]])
 
     return complex_events
 
 
-def pack_complex_vars(complex):
+def pack_complex_vars(complex_events):
     """Join complex events where the value is also a key
        Delete keys where values combined into prior event"""
 
-    extended = join_events(complex)
+    extended = join_events(complex_events)
 
-    for old in sorted(complex.keys()):
-        for new, joined in sorted(extended.iteritems()): # And new keys/values
+    for old in sorted(complex_events.keys()):
+        for new, joined in sorted(extended.iteritems()):  # And new keys/values
             if new != old:
                 for j in joined:
-                    if j in complex[old] and old in extended:
+                    if j in complex_events[old] and old in extended:
                         # print("Event %s and %s have value %s" % (old, new, j))
-                        extended.setdefault(old, []).extend(complex[new])
+                        extended.setdefault(old, []).extend(complex_events[new])
                         extended.pop(new, None)
 
     for k, v in sorted(extended.iteritems()):
         extended[k] = list(set(v))
-        # extended = list(OrderedDict.fromkeys(extended))
         extended[k].sort()
 
     return extended
@@ -134,44 +132,56 @@ def join_events(d):
     seen = []
     for k, l in sorted(d.iteritems()):
         for event in l:
-            if event in d: # Is this event also a key?
+            if event in d:  # Is this event also a key?
                 if event not in seen:
                     extended.setdefault(k, []).extend(d[event])
-                    if k != event: # Delete key if we're adding its contents to another key
+                    if k != event:  # Delete key if we're adding its contents to another key
                         extended.pop(event, None)
-                    # else: # else the event and key are the same, so we can remove it from list
-                    #     extended[k].remove(event)
                 seen.append(event)
 
     return extended
 
 
-def print_complex(complex, options, temp):
+def print_complex(complex_events, options, temp):
     sample = ntpath.basename(options.inFile).split("_")[0]
     out_file = os.path.join(options.out_dir, sample + '_stitched.txt')
 
     print("Writing stitched variants to: %s" % out_file)
-
     df = pd.read_csv(temp, delimiter="\t")
     df = df.sort_values(['event', 'chromosome1', 'bp1', 'chromosome2', 'bp2'])
+    events = defaultdict(lambda: defaultdict(int))
 
     for index, row in df.iterrows():
-        event, type, notes = row[['event', 'type', 'notes']]
-        for e, j in sorted(complex.iteritems()):
-            if event == e:
-                linked_events = "_".join(map(str, complex[event]))
-                configuration = ":".join(map(str, [e, linked_events]))
-                df.loc[index, 'configuration'] = configuration
-                df.loc[index, 'type'] = '_'.join(["COMPLEX", type])
+        event, sv_type, notes = row[['event', 'type', 'notes']]
+        for e, j in sorted(complex_events.iteritems()):
+            # if event == e:
+            #     linked_events = "_".join(map(str, complex_events[event]))
+            #     configuration = ":".join(map(str, [e, linked_events]))
+            #     df.loc[index, 'configuration'] = configuration
+            #     df.loc[index, 'type'] = '_'.join(["COMPLEX", sv_type])
             for joined in j:
                 if event == joined:
                     # print("Event %s is in %s - key: %s" % (event, joined, e))
-                    linked_events = "_".join(map(str, complex[e]))
+                    linked_events = "_".join(map(str, complex_events[e]))
                     configuration = ":".join(map(str, [event, linked_events]))
-                    df.loc[index, 'configuration'] = configuration
-                    df.loc[index, 'type'] = '_'.join(["COMPLEX", type])
+                    if len(complex_events[e]) > 1:
+                        df.loc[index, 'configuration'] = configuration
+                    df.loc[index, 'type'] = '_'.join(["COMPLEX", sv_type])
                     df.loc[index, 'event'] = e
 
+        events[df.loc[index, 'event']][df.loc[index, 'type']] += 1
+
+    # Finally, substitute "COMPELX_" for "" in events that are the same event chained together (DEL/DUPs)
+    for event in events:
+        if len(events[event]) == 1:
+            # print("%s Not complex" % (event))
+            val = events[event].keys()
+            # print("Old value = %s" % (val))
+            new = val[0].replace('COMPLEX_', '')
+            # print("New value = %s" % (new) )
+            df.loc[df['event'] == event, ['type']] = new
+
+    # print(json.dumps(events, indent=4, sort_keys=True))
 
     df = df.sort_values(['event', 'chromosome1', 'bp1', 'chromosome2', 'bp2'])
     os.remove(temp)
@@ -180,30 +190,15 @@ def print_complex(complex, options, temp):
 
 def main():
     parser = OptionParser()
-    parser.add_option("-i",
-        "--inFile", dest="inFile",
-        help="An annotated variants file produced by sv2gene "
-             "accepts both '_annotated_SVs.txt' and "
-             "'_reannotated_SVs.txt' files",
-        metavar="FILE")
-
-    parser.add_option("-w",
-        "--window", dest="window", action="store",
-        type=int,
-        help="The distance to search for connected breakpoints")
-
-    parser.add_option("-o",
-                      "--out_dir",
-                      dest="out_dir",
-                      action="store",
-                      help="Directory to write output to " +
-                           "[Default: '.']")
-
-    parser.set_defaults(window=1000, out_dir = os.getcwd())
-
+    parser.add_option("-i", "--inFile", dest="inFile", help="An annotated variants file produced by sv2gene "
+                                                            "accepts both '_annotated_SVs.txt' and "
+                                                            "'_reannotated_SVs.txt' files", metavar="FILE")
+    parser.add_option("-w", "--window", dest="window", action="store", type=int, help="The distance to search for connected breakpoints")
+    parser.add_option("-o", "--out_dir", dest="out_dir", action="store", help="Directory to write output to " + "[Default: '.']")
+    parser.set_defaults(window=1000, out_dir=os.getcwd())
     options, args = parser.parse_args()
 
-    print("Tying variants +/- %s" % (options.window))
+    print("Tying variants +/- %s" % options.window)
 
     if options.inFile is None:
         parser.print_help()
