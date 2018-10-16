992898cf01f46e24f1a18b778aa20c23a1950c26
nriddiford <nickriddiford@me.com>
2018-10-16 10:43:42 +0200

    Re-write for pandas; condense

--- script/svPlotter.py
+++ script/svPlotter.py
@@ -1,91 +1,78 @@
-#!/usr/bin/env python
-
 import sys
 import fnmatch
 import os
 from optparse import OptionParser
-
-parser = OptionParser()
-
-parser.add_option("-f",
-                  "--SV_calls",
-                  dest="SV_calls",
-                  help="SV calls file"
-                  )
-
-options, args = parser.parse_args()
+import pandas as pd
+import ntpath
 
 
-if not options.SV_calls:
-    parser.error('No input file provided')
+def select_cnvfile(sv_size, bp1, bp2, options):
+    big_window = '/Users/Nick_curie/Desktop/script_test/cnvPlotteR/data'
+    small_window = '/Users/Nick_curie/Desktop/script_test/cnvPlotteR/data/w500'
 
-variants_file = options.SV_calls
+    sample = ntpath.basename(options.SV_calls).split("_")[0]
 
-base_name = (os.path.splitext(variants_file)[0])
-out_base = base_name.split('_')[0]
+    # base_name = (os.path.splitext(options.SV_calls)[0])
+    # out_base = base_name.split('_')[0]
+    pattern = sample + '*.cnv'
 
-pattern = out_base + '*.cnv'
-
-big_window='/Users/Nick_curie/Desktop/script_test/cnvPlotteR/data'
-samll_window='/Users/Nick_curie/Desktop/script_test/cnvPlotteR/data/w500'
-
-# home
-# big_window='/Users/Nick/iCloud/Desktop/script_test/cnvPlotteR/data'
-# samll_window='/Users/Nick/iCloud/Desktop/script_test/cnvPlotteR/data/w500'
-
-
-def select_cnvfile(sv_size, bp1, bp2):
     if float(sv_size) <= 1000:
         start = bp1 - 100000
         if start < 0:
             start = 0
-        stop  = bp2 + 100000
+        stop = bp2 + 100000
         tick = 100000
 
-        files = os.listdir(samll_window)
+        files = os.listdir(small_window)
         for name in files:
             if fnmatch.fnmatch(name, pattern):
                 name = "data/w500/" + name
-                return[name, start, stop, tick]
+                return [name, start, stop, tick]
     else:
         start = bp1 - 1000000
         if start < 0:
             start = 0
-        stop  = bp2 + 1000000
+        stop = bp2 + 1000000
         tick = 1000000
 
         files = os.listdir(big_window)
         for name in files:
             if fnmatch.fnmatch(name, pattern):
                 name = "data/" + name
-                return[name, start, stop, tick]
+                return [name, start, stop, tick]
 
-def print_R_command(SV_calls_file):
+
+def print_R_command(options):
     """Generates R command that can be used to plot CNVs with https://github.com/nriddiford/cnvPlotteR"""
-    i=1
-    with open(SV_calls_file, 'U') as calls:
-        for l in calls:
-            parts = l.rstrip().split('\t')
+    df = pd.read_csv(options.SV_calls, delimiter="\t")
+    for idx, row in df.iterrows():
+        event, source, type, chrom1, bp1, chrom2, bp2, genotype, size, true = row[['event', 'source', 'type', 'chromosome1', 'bp1', 'chromosome2', 'bp2', 'genotype', 'length(Kb)', 'T/F']]
+        if genotype != 'somatic_tumour': continue
+        if options.true_only and true == 'F': continue
 
-            if i == 1 and parts[0] == 'event':
-                continue
-            elif parts[9] != 'somatic_tumour':
-                continue
+        if chrom1 == chrom2:
+            cnv_file, start, end, tick = select_cnvfile(size, bp1, bp2, options)
+            print("SV event: %s, type: %s, size: %s") % (event, type, size)
+            print("regionPlot(cnv_file=\"%s\", from=%s, to=%s, bp1=%s,bp2=%s,chrom=\"%s\", tick=%s, title=\"%sKb %s on %s\")") % (cnv_file, start, end, bp1, bp2, chrom1, tick, size, type, chrom1)
 
-            event, source, type, chrom1, bp1, chrom2, bp2 = parts[0:7]
-            bp1 = int(bp1)
-            bp2 = int(bp2)
-            # size = parts[10] change for new genotype col
-            size = parts[11]
-            # if chrom1 == chrom2 and type != 'INV':
-            if chrom1 == chrom2:
+def main():
+    parser = OptionParser()
 
-                fields = select_cnvfile(size, bp1, bp2)
-                cnv_file = fields[0]
-                start = fields[1]
-                end = fields[2]
-                tick = fields[3]
-                print("SV event: %s, type: %s, size: %s") % (event, type, size)
-                print("regionPlot(cnv_file=\"%s\", from=%s, to=%s, bp1=%s,bp2=%s,chrom=\"%s\", tick=%s, title=\"%sKb %s on %s\")") % (cnv_file, start, end, bp1, bp2, chrom1, tick, size, type, chrom1)
+    parser.add_option("-f", "--SV_calls", dest="SV_calls", help="SV calls file")
+    parser.add_option("-t", "--true_only", dest="true_only", action="store_true", help="Don't print calls marked as FP")
 
-print_R_command(variants_file)
+    options, args = parser.parse_args()
+
+    if options.SV_calls is None:
+        parser.print_help()
+        print()
+    else:
+        try:
+            print_R_command(options)
+        except IOError as err:
+            sys.stderr.write("IOError " + str(err) + "\n")
+            return
+
+
+if __name__ == "__main__":
+    sys.exit(main())
