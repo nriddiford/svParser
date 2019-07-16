3059451956b3e981c479c480cd0181b7b044e9b2
nriddiford <nickriddiford@me.com>
2019-07-16 18:19:34 +0200

    Improve and tidy up

--- script/getCNVs.py
+++ script/getCNVs.py
@@ -1,53 +1,86 @@
 #!/usr/bin/python
 
-import sys
 from optparse import OptionParser
-import os
+import pandas as pd
+import os, sys
 
-parser = OptionParser()
 
-parser.add_option("-f",
-                  "--annotated_files",
-                  dest="SV_calls",
-                  help="SV calls file"
-                  )
-
-options, args = parser.parse_args()
-
-if not options.SV_calls:
-    parser.error('No input file provided')
-
-base_name = (os.path.splitext(options.SV_calls)[0])
-sample = base_name.split('_')[0]
-outfile = sample + "_" + "CNVs.txt"
-
-script_dir = os.path.dirname(__file__)
-path = os.path.join(script_dir, '../data/cnv')
-output = os.path.join(path, outfile)
 
 def getCNVs(calls):
     cnvs = []
-    with open(calls, 'U') as sv_calls_file:
-        for l in sv_calls_file:
-            parts = l.rstrip().split('\t')
 
-            if "cnv" in parts[1].lower():
-                try:
-                    depth = parts[17]
-                except IndexError:
-                    depth = '-'
+    breakpoints = pd.read_csv(calls, delimiter="\t", index_col=False, na_filter=False)
+    breakpoints = breakpoints[breakpoints['source'].str.contains('cnv', case=False)]
 
-                line = [parts[3], parts[4], parts[6], parts[2], parts[11], depth]
+    for index, row in breakpoints.iterrows():
+        length = round((row['bp2']-row['bp1'])/1e3, 1)
+        line = list( map(str, [row['chromosome1'], row['bp1'], row['bp2'], row['type'], length, row['log2(cnv)']]) )
+        line = '\t'.join(line)
+        cnvs.append(line)
 
-                line = '\t'.join(line)
-                cnvs.append(line)
+
+    # with open(calls, 'U') as sv_calls_file:
+    #     for l in sv_calls_file:
+    #         parts = l.rstrip().split('\t')
+    #
+    #         if "cnv" in parts[1].lower():
+    #             try:
+    #                 depth = parts[18]
+    #             except IndexError:
+    #                 depth = '-'
+    #
+    #             line = [parts[3], parts[4], parts[6], parts[2], parts[11], depth]
+    #
+    #             line = '\t'.join(line)
+    #             cnvs.append(line)
+
     return(cnvs)
 
-def printCNVs(cnvs, output):
+
+def printCNVs(cnvs, options):
+    base_name = (os.path.basename(options.annotated_variants))
+    outfile = base_name.split('_')[0] + "_" + "CNVs.txt"
+    output = os.path.join(options.cnv_dir, outfile)
+
+    print("Writing CNVs to: '%s'" % output)
+
     with open(output, 'w') as cnv_out:
         for l in cnvs:
             cnv_out.write("%s\n" % l)
 
-cnvs = getCNVs(options.SV_calls)
-if cnvs:
-    printCNVs(cnvs, output)
+
+def get_args():
+    parser = OptionParser()
+
+    parser.add_option("-f",
+                      "--annotated_variants",
+                      dest="annotated_variants",
+                      help="annotated variants file")
+
+    parser.add_option("-d",
+                      "--cnv_dir",
+                      dest="cnv_dir",
+                      action="store",
+                      help="Directory to write cnvs to")
+
+    options, args = parser.parse_args()
+
+    if not options.annotated_variants or not options.cnv_dir:
+        parser.print_help()
+        exit("err: Must provide both '-f' and '-d' options. Exiting")
+    return options, args
+
+
+def main():
+        options, args = get_args()
+
+        cnvs = getCNVs(options.annotated_variants)
+
+        print(cnvs)
+
+        if cnvs:
+            printCNVs(cnvs, options)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
