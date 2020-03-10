e1f765466642076ca6ae9580379b343e7e44f3e2
nriddiford <nickriddiford@me.com>
2020-03-10 10:21:07 +0100

    Add header manually

--- script/merge_files.py
+++ script/merge_files.py
@@ -10,17 +10,16 @@ def merge_samples(options):
         options.directory = os.getcwd()
 
     out_file = os.path.join(options.directory, options.out_file)
-    print("Writing %s") % (out_file)
-
-    print(options.directory)
-    print(options.extension)
+    print("Writing '%s'") % (out_file)
     options.extension = "*" + options.extension
-    print(options.extension)
-
     print(fnmatch.filter(os.listdir(options.directory), options.extension))
     files = fnmatch.filter(os.listdir(options.directory), options.extension)
-    header = list(pd.read_csv(os.path.join(options.directory, files[0]), delimiter="\t", nrows=1).columns)
-    header.insert(0, 'sample')
+    # header = list(pd.read_csv(os.path.join(options.directory, files[0]), delimiter="\t", nrows=1).columns)
+    header = ['sample', 'event', 'source', 'type', 'chromosome1', 'bp1', 'chromosome2', 'bp2', 'split_reads',
+                         'disc_reads', 'genotype', 'id', 'length(Kb)', 'position', 'configuration', 'allele_frequency',
+                         'log2(cnv)', 'microhomology', 'inserted_seq', 'consensus', 'contig_sequence', 'mechanism',
+                         'bp1_locus', 'bp2_locus', 'affected_genes', 'status', 'notes', 'snp_freq']
+    # header.insert(0, 'sample')
 
     with open(out_file, 'w') as all_out:
         all_out.write('\t'.join(header) + '\n')
@@ -28,9 +27,21 @@ def merge_samples(options):
             print(os.path.join(options.directory, f))
             sample = ntpath.basename(f).split("_")[0]
             df = pd.read_csv(os.path.join(options.directory, f), delimiter="\t", index_col=False, na_filter=False)
+
+            if 'inserted_seq' not in df:
+                df['inserted_seq'] = '-'
+                df['contig_sequence'] = '-'
+
+            df['sample'] = sample
+
+            # df = df[new_order]
+            df = df.reindex(columns=header)
+            # df = df.drop([0])
+            # df.to_csv(all_out, sep="\t", index=False)
+
             for idx, row in df.iterrows():
-                all_out.write(sample + '\t')
-                all_out.write('\t'.join(map(str, row)) + '\n')
+                if row['status'] not in ['F', 'aF']:
+                    all_out.write('\t'.join(map(str, row)) + '\n')
 
 
 def get_args():
